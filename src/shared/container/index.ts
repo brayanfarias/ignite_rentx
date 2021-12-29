4ccc173d3164af404dedef4c4d9e1ac2d6e1a0bd
bpjfarias <bpjfarias@gmail.com>
2021-12-29 18:27:43 -0300

    caso-de-uso-do-cadastro-de-imagens-do-carro

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import UserRepositoryImpl from "@modules/accounts/infra/typeorm/repositories/UserRepositoryImpl";
 import IUsersRepository from "@modules/accounts/repositories/IUsersRepository";
 import CarRepositoryImpl from "@modules/cars/infra/typeorm/repositories/CarRepositoryImpl";
+import CarsImageRepositoryImpl from "@modules/cars/infra/typeorm/repositories/CarsImageRepositoryImpl";
 import CategoryRepositoryImpl from "@modules/cars/infra/typeorm/repositories/CategoryRepositoryImpl";
 import SpecificationRepositoryImpl from "@modules/cars/infra/typeorm/repositories/SpecificationRepositoryImpl";
 import ICarRepository from "@modules/cars/repositories/ICarRepository";
+import ICarsImageRepository from "@modules/cars/repositories/ICarsImageRepository";
 import ICategoryRepository from "@modules/cars/repositories/ICategoryRepository";
 import ISpecificationRepository from "@modules/cars/repositories/ISpecificationRepository";
 import { container } from "tsyringe";
@@ -26,4 +28,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarRepository>(
   "CarRepositoryImpl",
   CarRepositoryImpl
+);
+
+container.registerSingleton<ICarsImageRepository>(
+  "CarsImagesRepositoryImpl",
+  CarsImageRepositoryImpl
 );
